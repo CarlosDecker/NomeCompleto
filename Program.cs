65794e218aa66b75f9b81ebde65ecfe8eca9dc48
Carlos Decker <carlosdecker022@gmail.com>
2022-09-09 22:29:55 -0300

    Exercício finalizado

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿    System.Console.Write("Digite seu nome e sobrenome: ");
+
+string Nome = Console.ReadLine()!;
+
+string PrimeiroNome = Nome.Substring(0, Nome.IndexOf(" "));
+string Sobrenome = Nome.Substring(Nome.IndexOf(" ") + 1);
+
+System.Console.WriteLine();
+    System.Console.WriteLine("Nome completo: " + PrimeiroNome + " " + Sobrenome);
+System.Console.WriteLine();
+    System.Console.WriteLine("Nome de catálogo: " + Sobrenome + ", " + PrimeiroNome);
+System.Console.WriteLine();
+    System.Console.WriteLine("Pressione uma tecla para fechar...");
+Console.ReadKey();
